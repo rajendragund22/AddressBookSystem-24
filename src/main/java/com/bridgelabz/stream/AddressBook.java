6f4ff5316fef17daab5a42e80245ca8fd220d1e4
RajendraGund <rajendragund2015@gmail.com>
2021-09-22 18:02:54 +0530

    Added : Program for the create contact in address book

---
+++ src/main/java/com/bridgelabz/stream/AddressBook.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.stream;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class AddressBook {
+    Scanner scan=new Scanner(System.in);
+    ArrayList<Contacts> list=new ArrayList<>();
+}
--- src/main/java/com/bridgelabz/stream/AddressBookMain.java
+++ src/main/java/com/bridgelabz/stream/AddressBookMain.java
@@ -3,5 +3,6 @@ package com.bridgelabz.stream;
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome into the Address Book System in Stream!!!!");
+        AddressBook addressBook=new AddressBook();
     }
 }
