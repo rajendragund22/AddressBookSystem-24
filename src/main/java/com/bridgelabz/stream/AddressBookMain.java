4e0e5cffc69282fc71285274afdae8385b0eca97
RajendraGund <rajendragund2015@gmail.com>
2021-09-22 17:17:58 +0530

    Added : Welcome message for the user

---
+++ src/main/java/com/bridgelabz/stream/AddressBookMain.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.stream;
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome into the Address Book System in Stream!!!!");
+    }
+}
