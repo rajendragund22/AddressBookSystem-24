a0bd2919869e5352fb067fd28c9acdf6e4ef03af
RajendraGund <rajendragund2015@gmail.com>
2021-09-24 17:09:05 +0530

    Added : Program for the add new contact in the address book using console

--- src/main/java/com/bridgelabz/stream/AddressBookMain.java
+++ src/main/java/com/bridgelabz/stream/AddressBookMain.java
@@ -3,6 +3,8 @@ package com.bridgelabz.stream;
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome into the Address Book System in Stream!!!!");
-        AddressBook addressBook=new AddressBook();
+        AddressBook addressBook = new AddressBook();
+        addressBook.operation();
     }
+
 }
