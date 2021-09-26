9c10f2c051a220200658be6018152dc76dca18de
RajendraGund <rajendragund2015@gmail.com>
2021-09-26 10:35:57 +0530

    Added : Program for the count person by the city or state in the address book system

--- src/main/java/com/bridgelabz/stream/AddressBook.java
+++ src/main/java/com/bridgelabz/stream/AddressBook.java
@@ -19,7 +19,8 @@ public class AddressBook {
         do {
 
             System.out.println("Choose Operation you want to do");
-            System.out.println("1. Add\t2.Edit\t3.Delete\t4.sortCity\t5.sortState\t6.show");
+            System.out.println("1. Add\t2.Edit\t3.Delete\t4.sortCity\t5.sortState\t6.show\t7.Count");
+
             switch (scan.nextInt()) {
                 case 1:
                     add();
@@ -36,6 +37,8 @@ public class AddressBook {
                     sortState();
                 case 6:
                     print();
+                case 7:
+                    countList();
                 default:
                     status = false;
             }
@@ -143,6 +146,11 @@ public class AddressBook {
         while (it.hasNext()) {
             System.out.println(it.next());
         }
+    }
+
+    public void countList() {
+        Long total = list.stream().count();
+        System.out.println(total);
     }
 
     public void sortCity() {
